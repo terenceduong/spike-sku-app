210e8734865d4608bd842fbb564c8fd4689baad0
Terence <terencee.d@hotmail.com>
2023-09-22 13:47:58 +1000

    Use example server to get css working

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -1,28 +1,93 @@
-import 'braid-design-system/reset';
-
 import { renderToString } from 'react-dom/server';
+// import fs from 'fs/promises';
 
 import App from '../App/App';
 
-// import middleware from './middleware';
-// import template from './template';
+const initialResponseTemplate = ({
+  headTags,
+}: {
+  headTags: string;
+}) => /* html */ `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>hello-world</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    ${headTags}
+`;
+
+const template = ({
+  headTags,
+  bodyTags,
+  app,
+  extraScripts,
+}: {
+  headTags: string;
+  bodyTags: string;
+  app: string;
+  extraScripts?: string[];
+}) => /* html */ `
+      ${headTags}
+    </head>
+    <body>
+      <div id="app">${app}</div>
+      ${extraScripts?.join('\n')}
+      ${bodyTags}
+    </body>
+  </html>
+`;
 
 export default () => ({
-  renderCallback: ({ SkuProvider, getBodyTags, getHeadTags }, req, res) => {
-    console.log('rendercallback in server.tsx');
+  renderCallback: async (
+    // { SkuProvider, getBodyTags, flushHeadTags, registerScript },
+    {
+      SkuProvider,
+      getBodyTags,
+      flushHeadTags,
+    }: {
+      getBodyTags: () => string;
+      flushHeadTags: () => string;
+      SkuProvider: any;
+    },
+    req,
+    res,
+  ) => {
+    // const extraScripts = [
+    //   `<script src="https://code.jquery.com/jquery-3.5.0.slim.min.js"></script>`,
+    //   `<script>console.log('Hi');</script>`,
+    // ];
+
+    // extraScripts.forEach((script) => registerScript(script));
+
+    res.status(200).write(
+      initialResponseTemplate({
+        headTags: flushHeadTags(),
+      }),
+    );
+    await Promise.resolve();
+
     const app = renderToString(
       <SkuProvider>
-          <App environment="development" />
+        <App environment="development" />
       </SkuProvider>,
     );
-    res.send(app);
-    // res.send(
-    //   template({ headTags: getHeadTags(), bodyTags: getBodyTags(), app }),
-    // );
+    res.write(
+      template({
+        headTags: flushHeadTags(),
+        bodyTags: getBodyTags(),
+        app,
+        // extraScripts,
+      }),
+    );
+    res.end();
   },
-  //   middleware: [],
-  onStart: (app) => {
-    console.log('My app started 👯‍♀️!');
-    app.keepAliveTimeout = 20000;
-  },
+  // onStart: async () => {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     await fs.writeFile(
+  //       './started.txt',
+  //       "Server started, here's your callback",
+  //     );
+  //   }
+  // },
 });
